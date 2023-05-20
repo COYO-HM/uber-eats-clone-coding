fdeeec9e5ce4b050c3126ea7348fae1d03257589
COYO <dayeon.yoon@gmail.com>
2023-05-20 22:29:28 +0900

    BACKEND: DB SETUP

--- nomad-coder-nuber-eats-backend/src/app.module.ts
+++ nomad-coder-nuber-eats-backend/src/app.module.ts
@@ -1,8 +1,40 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      // production 환경일 때는 Config Module이 환경변수 파일을 무시한다.
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: false,
+      logging: true,
+    }),
+    ,
+    GraphQLModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })

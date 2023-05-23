8129e292cfc489be790bad73d3894b8008bdc0e4
COYO <dayeon.yoon@gmail.com>
2023-05-24 01:01:09 +0900

    BACKEND: Create Account Mutation

--- nomad-coder-nuber-eats-backend/src/common/entities/core.entity.ts
+++ nomad-coder-nuber-eats-backend/src/common/entities/core.entity.ts
@@ -4,15 +4,22 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { number } from 'joi';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class CoreEntity {
   @PrimaryGeneratedColumn()
+  @Field((type) => number)
   id: number;
 
   @CreateDateColumn()
+  @Field((type) => Date)
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Field((type) => Date)
   updatedAt: Date;
 }
--- nomad-coder-nuber-eats-backend/src/users/entities/user.entity.ts
+++ nomad-coder-nuber-eats-backend/src/users/entities/user.entity.ts
@@ -1,16 +1,22 @@
 import { Column, Entity } from 'typeorm';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from '../../common/entities/core.entity';
 
 type UserRole = 'client' | 'delivery' | 'owner';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
   @Column()
+  @Field((type) => String)
   email: string;
 
   @Column()
+  @Field((type) => String)
   password: string;
 
   @Column()
+  @Field((type) => String)
   role: UserRole;
 }
